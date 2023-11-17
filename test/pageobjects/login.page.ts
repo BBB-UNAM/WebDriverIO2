82c1589121aa96318958f804e259622cb8e97dc7
Alberto Picasso <picassoa12@gmail.com>
2023-11-17 14:01:28 -0600

    Add new functions

--- test/pageobjects/login.page.ts
+++ test/pageobjects/login.page.ts
@@ -1,36 +1,61 @@
 import { $ } from '@wdio/globals'
 import Page from './page.js';
+import * as assert from 'assert';
 
 class LoginPage extends Page {
-
-    public get inputUsername () {
-        return $('~input-email');
-    }
-
-    public get inputPassword () {
-        return $('~input-password');
-    }
-
-    public get btnSubmit () {
-        return $('~button-LOGIN');
-    }
+    //---------------------------------------Login Elemnts-----------------------------------------
+    public get inputUsername () { return $('~input-email'); }
+    public get inputPassword () { return $('~input-password'); }
+    public get btnSubmit () { return $('~button-LOGIN'); }
 
     public get successMesage () {
-        return $('ndroid=new UiSelector().resourceId("android:id/message")'); //#android:id/message
+        return $('android=new UiSelector().resourceId("android:id/message")'); //#android:id/message
     }
     public get okButton () {
         return $('android=new UiSelector().resourceId("android:id/button1")');
     }
-
+    //---------------------------------------Sing up elements-----------------------------------------
+    public get loginTab () { return $('~button-login-container'); }
+    public get signUpTab () { return $('~button-sign-up-container'); }
+    public get emailSignUp () { return $('~input-email'); }
+    public get passSignUp () { return $('~input-password'); }
+    public get repeatPassSignUp () { return $('~input-repeat-password'); }
+    public get btnSighUp () { return $('~button-SIGN UP'); }
+    //---------------------------------------Methods-----------------------------------------
     public async login (username: string, password: string) {
+        //Steps to login to the app
         await this.inputUsername.setValue(username);
         await this.inputPassword.setValue(password);
         await this.btnSubmit.click();
-        (await this.okButton).click();
-        console.log("ok clicked");
+    }
 
-        const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-        await sleep(3000);
+    public async validateLogin(){
+        (await this.successMesage).waitForDisplayed({ //wait a maximum of five seconds to succed message is displayed
+            timeout: 5000,                            // with intervals of two seconds.
+            interval: 500, 
+            reverse: false,
+            timeoutMsg: 'El elemento no existió en el tiempo especificado'
+        });
+
+        const message = await (await this.successMesage).getText(); //get the content of the message.
+
+        await assert.strictEqual("You are logged in!".includes(message),true,"Message not displayed"); //hard assert example
+
+        try { //soft assert example
+            await assert.strictEqual("You are logged in!".includes(message),true);
+        } catch(error){
+            console.log("This message isnt the expected: ",message);
+        }
+
+        (await this.okButton).click(); // click on OK button
+    }
+
+    public async signUp(username: string, password: string){
+        await (await this.signUpTab).click();
+        await (await this.emailSignUp).setValue(username);
+        await (await this.passSignUp).setValue(password);
+        await (await this.repeatPassSignUp).setValue(password);
+        await (await this.btnSighUp).click();
     }
 }
 
--- test/specs/basics.spec.ts
+++ test/specs/basics.spec.ts
@@ -1,27 +1,19 @@
 import LoginPage from '../pageobjects/login.page.js'
 
 describe('My Login application', () => {
-    it('this is test1', async () => {
-        console.log("Test1");
-    });
-
-    it('this is test2', async () => {
-        console.log("Test2");
-    });
 
     it('Verify Login', async () => {
         await (await LoginPage.loginButton).isDisplayed();
         await (await LoginPage.loginButton).click();
         await LoginPage.login('tomsmith@mail.com', 'SuperSecretPassword!');
-        //const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-        //await sleep(8000);
-        //await driver.switchToFrame($('#android:id/content'));
-        //await driver;
-        //console.log("frame switched");
-        //const contexts = await browser.getContexts();
-        //console.log(contexts[0]);
-        //await driver.switchContext(contexts[0]);
-        //const iframe = $('#android:id/content');
-        //iframe.switchToFrame();
+        await LoginPage.validateLogin();
     });
+
+    it.only('Verify Sign Up', async () => {
+        await (await LoginPage.loginButton).isDisplayed();
+        await (await LoginPage.loginButton).click();
+        await (await LoginPage.signUpTab).click();
+        await LoginPage.signUp("picassoa12@gmail.com","qwertyuiop");
+    });
+
 })
